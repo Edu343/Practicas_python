8977656de458d014cb7e51a2714694a756fb9ac5
Edu343 <142481337+Edu343@users.noreply.github.com>
2023-08-31 13:58:47 -0600

    Hola

---
+++ Python/01_Variables.py
@@ -0,0 +1,45 @@
+print("hello")
+
+
+name, surname, edad= "Eduardo", "Del Rio", "17"
+
+print("Me llamo:", name, surname, ".Tengo la edad de:",edad)
+
+
+#Input
+
+name= input("Tu nombre plox: ")
+
+edad= input ("Mtete tu edad chi? ")
+
+print("Encantado de conocerte,", name, "Con edad de: ", edad)
+
+"""
+
+
+
+"""
+print("Suma dos números que tu quieras")
+
+
+
+num1= float(input("Pon el primer número: "))
+
+
+num2= float(input("Pon el segundo número: "))
+
+
+
+resultado= num1 + num2
+
+print("Tu resultado es: ", resultado)
+
+
+
+#Confuciones en variables
+#Forzando el tipo
+
+address: str = "Mi dirección"
+address= 32
+print(address)
+print(type(address))
